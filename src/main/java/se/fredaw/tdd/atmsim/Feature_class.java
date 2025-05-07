73f51272c20ce8e91c678838322b4e325b6075c9
Fredrik <181095176+HuckerDuck@users.noreply.github.com>
2025-05-07 10:16:20 +0200

    Feature Update
    - Added Pom Dependancys
    - Added a Test folder with a seprate folder to use in the future

---
+++ src/main/java/se/fredaw/tdd/atmsim/Feature_class.java
@@ -0,0 +1,8 @@
+package se.fredaw.tdd.atmsim;
+
+public class Feature_class {
+
+    public void feature_method() {
+
+    }
+}
---
+++ src/test/java/se/fredaw/tdd/atmsim/Feature_classTest.java
@@ -0,0 +1,6 @@
+package se.fredaw.tdd.atmsim;
+
+import static org.junit.jupiter.api.Assertions.*;
+class Feature_classTest {
+  
+}
