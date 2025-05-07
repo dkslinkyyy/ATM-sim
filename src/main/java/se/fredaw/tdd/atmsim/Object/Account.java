9b77eacaab16cd7f420bc49700ae90866c7c3088
Fredrik <181095176+HuckerDuck@users.noreply.github.com>
2025-05-07 10:47:53 +0200

    Feature Update
    - Test Class intial - PASS
    - Added 2 simple feature and tested that they worked.
    - Added a acccount and a beforeEach that will set a initial amount before the actual test will start
    
    Co-Authored-By: Dawid Kowalski <48870259+dkslinkyyy@users.noreply.github.com>

---
+++ src/main/java/se/fredaw/tdd/atmsim/Object/Account.java
@@ -0,0 +1,15 @@
+package se.fredaw.tdd.atmsim.Object;
+
+public class Account {
+
+    private int balance = 0;
+
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    public int getBalance() {
+        return balance;
+    }
+
+}
--- src/test/java/se/fredaw/tdd/atmsim/Object/ATMServiceTest.java
+++ src/test/java/se/fredaw/tdd/atmsim/Object/ATMServiceTest.java
@@ -1,14 +1,16 @@
-package se.fredaw.tdd.atmsim;
+package se.fredaw.tdd.atmsim.Object;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ATMFeatureTest {
+class ATMServiceTest {
 
   private ATMService atmService;
   private Account account;
 
 
+  // When the test is started, the balance is set to 1000
   @BeforeEach
     void setup() {
       atmService = new ATMService();
@@ -16,8 +18,10 @@ class ATMFeatureTest {
       account.setBalance (1000);
 
 
+
   }
 
+  //Here we test if the withdraw function is correct
   @Test
     void withdrawTest() {
     atmService.withdraw(account, 500);
@@ -27,9 +31,10 @@ class ATMFeatureTest {
 
   }
 
+  //Here we try to add money to the account
   @Test
     void depositTest(){
-    atmService.desposit(account, 500);
+    atmService.deposit(account, 500);
     assertEquals(1500, account.getBalance());
   }
 
