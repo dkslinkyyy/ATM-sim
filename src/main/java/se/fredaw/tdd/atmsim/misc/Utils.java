6cc204037c74659adca25e25dffddda77a5a5eb9
dkslinkyyy <slinky.sthlm@gmail.com>
2025-05-13 13:08:48 +0200

    Test: Utils throwWith function test - initial result: fail

--- src/main/java/se/fredaw/tdd/atmsim/misc/Utils.java
+++ src/main/java/se/fredaw/tdd/atmsim/misc/Utils.java
@@ -1,9 +1,17 @@
 package se.fredaw.tdd.atmsim.misc;
 
+import java.util.function.Supplier;
+
 public class Utils {
 
 
     public static void print(Message message) {
         System.out.println(message.getColor().getColorCode() + message.getPlaceholder() + Color.RESET.getColorCode());
+    }
+
+    public static void throwIf(boolean condition, Supplier<? extends RuntimeException> exceptionSupplier) {
+        if (condition) {
+            throw exceptionSupplier.get();
+        }
     }
 }
---
+++ src/test/java/se/fredaw/tdd/atmsim/misc/UtilsTest.java
@@ -0,0 +1,31 @@
+package se.fredaw.tdd.atmsim.misc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UtilsTest {
+    @Test
+    void throwWith_shouldThrowGivenExceptionWithMessage() {
+        // Arrange
+        Message msg = Message.INSUFFICIENT_BALANCE;
+
+        RuntimeException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Utils.throwWith(msg, IllegalArgumentException::new)
+        );
+
+        assertEquals(msg.getPlaceholder(), exception.getMessage());
+    }
+
+    @Test
+    void throwWith_shouldThrowCustomExceptionWithCustomMessage() {
+        Message msg = Message.INSUFFICIENT_BALANCE;
+
+        RuntimeException exception = assertThrows(
+                IllegalStateException.class,
+                () -> Utils.throwWith(msg, m -> new IllegalStateException("error: " + m))
+        );
+
+        assertEquals("ATM Error: " + msg.getPlaceholder(), exception.getMessage());
+    }
+}
